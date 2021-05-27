b87aee0b5ad90df372084c1a7a1ce50ad9a85874
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-05-27 14:40:35 +0300

    Added constructor and state to the React class

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  //JS object constructor
+  constructor(props) {
+    super(props); //super is the React constructor
+    // THIS IS THE ONLY TIME we do direct assignment to this.state!
+    this.state = { lat: null }; //latitude
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        this.setState({ lat: position.coords.latitude});
+        console.log(position);
+      },
       (err) => console.log(err)
     );
+  }
 
-    return <div>Latitude: </div>;
+  // React says we have to define render
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 

b445c0516b8c35c6cc72d78472b1c926bf2bb867
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-06-29 14:07:10 +0300

    Added more handling to the SeasonDisplay wheather it's winter or summer

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -6,13 +6,20 @@ const getSeason = (lat, month) => {
   } else {
     return lat > 0 ? "winter" : "summer";
   }
-}
+};
 
 const SeasonDisplay = (props) => {
   console.log(props.lat);
   const season = getSeason(props.lat, new Date().getMonth());
   console.log(season);
-  return <div>Season Display</div>;
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
